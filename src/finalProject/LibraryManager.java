9d57b6d02434f3ec27a9f64ef875fed2c743ecff
wheel <wheel@Lawrences>
2026-03-30 12:48:15 -0700

    added temp main menu in main to see progress of program

--- src/finalProject/LibraryManager.java
+++ src/finalProject/LibraryManager.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException; 
-
+/*
+ * Author: Lawrence Oro
+ * Class: LibraryManager.java
+ * 
+ * 
+ * Description:
+ This class manages the collection of books. 
+ Using an ArrayList data structure to store our Book objects in memory 
+ I chose to separate this logic from our main to make for better abstraction practice encapsulating the book collection
+ ensuring that the data is only modified through my methods
+ This will also allow for reusability for an app version in version 2.0 
+ */
 public class LibraryManager {
 	//defining the data structure(ArrayList)
 	private ArrayList<Book> myLibrary = new ArrayList();
@@ -17,7 +28,7 @@ public class LibraryManager {
 				String line = scanner.nextLine();
 				if(line.isEmpty()) 
 					continue;
-				
+				//parsing our data to make for easy readability
 				String[] data = line.split(",");
 				if (data.length == 5) {
 					//creating our data unit(book)
@@ -35,6 +46,19 @@ public class LibraryManager {
 			//using javas built in getMessage() to give user better understanding of error if file were to crash
 			System.out.println("Could not load library :( ." + e.getMessage());
 		}
+	}
+	
+	public void displayLibrary() {
+		System.out.println("\n--- Current Library Collection ---");
+		if (myLibrary.isEmpty()) {
+			System.out.println("The library is empty. Please add some books!");
+		}else {
+			for(Book b : myLibrary) {
+				System.out.println(b.toString());
+			}
+		}
+		System.out.println("----------------------------------------\n");
+		
 	}
 }
 			
--- src/finalProject/Main.java
+++ src/finalProject/Main.java
@@ -1,10 +1,60 @@
 package finalProject;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//initializing our lib managaer class and scanner for input purposes 
+		LibraryManager myManager = new LibraryManager();
+		Scanner input = new Scanner(System.in);
+		boolean running = true;
+		
+		System.out.println("Welcome to your Library Management System! ");
+		
+// loading users existing data
+		System.out.println("Loading your library...");
+		myManager.loadBooks();
+		
+		//loop to continue running program until user chooses otherwise
+		while(running) {
+			System.out.println("\n============ Main Menu ==============");
+			System.out.println("Please Choose One of the Following: ");
+			System.out.println("1. View Library");
+			System.out.println("2. Add Book to Library (In Progress)");
+			System.out.println("3. Search Book in Library (In Progress)");
+			System.out.println("4. Exit Program");
+			
+			String choice = input.nextLine();
+			
+			switch (choice) {
+			case "1":
+				//displays library 
+				myManager.displayLibrary();
+				break;
+				
+			case "2":
+				System.out.println("Feature under construction! Building 'add' logic here.");
+				break;
+			
+			case "3":
+				System.out.println("Feature under construction! Building 'search' logic here.");
+				break;
+			
+			case "4":
+				System.out.println("Thank you for using the Library Management. Goodbye!");
+				running = false;
+				break;
+			
+			default: 
+				System.out.println("Invalid selction! Please try again!");
+				break;
+			}
+			
+		}
+		//closing scanner
+		input.close();
 	}
 
 }
